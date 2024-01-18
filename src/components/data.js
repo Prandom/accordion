d3ce868fbb68fb120cee597d390aaacf3fcc5ba9
Prannoy <cprannoy@gmail.com>
2024-01-18 10:22:52 +0530

    Commit 1: Accordion

---
+++ src/components/data.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+      id: 1,
+      question: 'What is JavaScript?',
+      answer: 'JavaScript is a programming language that enables interactive web pages. It is widely used for client-side development.'
+    },
+    {
+      id: 2,
+      question: 'What is an array in JavaScript?',
+      answer: 'An array is a data structure that stores a collection of elements, each identified by an index or a key.'
+    },
+    {
+      id: 3,
+      question: 'How to declare a variable in JavaScript?',
+      answer: 'You can declare a variable using the "var," "let," or "const" keyword, followed by the variable name.'
+    },
+    {
+      id: 4,
+      question: 'What is the DOM?',
+      answer: 'The Document Object Model (DOM) is a programming interface for web documents. It represents the structure of a document as a tree of objects.'
+    },
+    {
+      id: 5,
+      question: 'How to add an event listener in JavaScript?',
+      answer: 'You can add an event listener using the "addEventListener" method. It allows you to respond to events like clicks, keypress, etc.'
+    }
+  ];
+
+  export default data;
+  
+ 
